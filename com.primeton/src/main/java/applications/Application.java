8691306bf6e9c239d1462e89275c574edcf6ed07
dongcc <dongcc@primeton.com>
2016-08-03 10:37:09 +0800

    去除一些不必要内容

--- com.primeton/src/main/java/applications/Application.java
+++ com.primeton/src/main/java/applications/Application.java
@@ -2,8 +2,6 @@ package applications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
