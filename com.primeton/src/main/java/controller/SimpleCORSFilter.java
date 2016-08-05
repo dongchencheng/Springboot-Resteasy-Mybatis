7d6aeeee41787f04e50e69ccb73d3853a11ed631
dongcc <dongcc@primeton.com>
2016-08-05 10:34:29 +0800

    跨域访问设置

---
+++ com.primeton/src/main/java/controller/SimpleCORSFilter.java
@@ -0,0 +1,35 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+/**   
+ * @Description: 跨域设置
+ * @author dongcc (mailto:dongcc@primeton.com)
+ * @date 2016年8月5日 上午10:13:40 
+ * @version V1.0   
+ */
+
+@Component
+public class SimpleCORSFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE,PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {}
+
+    public void destroy() {}
+
+}
